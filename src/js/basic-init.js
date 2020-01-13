8e4b9627b4036dbc9c6fe3bdedd9e4cd56731aec
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-01-13 21:00:07 +0200

    Завершил верстку первого экрана

--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
     'use strict';
 
+    // offer slider
+    $('.offer-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
 }); // end ready
